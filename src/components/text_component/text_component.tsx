895d7d4f6a7895ff3a9a91bb9730d218e7a4f491
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-10-26 11:10:12 +0100

    feat: add text component

---
+++ src/components/text_component/text_component.tsx
@@ -0,0 +1,23 @@
+
+import { type VariantProps } from "class-variance-authority";
+import { type HTMLAttributes, type JSX } from "react";
+import { twMerge } from "tailwind-merge";
+import { textVariants } from "./text_variants";
+
+type TextProps = HTMLAttributes<HTMLParagraphElement> &
+  VariantProps<typeof textVariants> & {
+    as?: "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "div";
+  };
+
+export const TextComponent =
+  ({ as: Component = "p", className, variant, ...props }: TextProps): JSX.Element => {
+    return (
+      <Component
+        className={twMerge(textVariants({ variant }), className)}
+        {...props}
+      />
+    );
+  }
+;
+
+TextComponent.displayName = "Text";
