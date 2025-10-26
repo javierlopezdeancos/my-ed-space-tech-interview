d5c87610caae268bd5c3b7070bb58ceb1258a9dd
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-10-26 11:11:57 +0100

    feat: add auth context

---
+++ src/contexts/auth_context.tsx
@@ -0,0 +1,20 @@
+import { createContext, useState, type JSX, type ReactNode } from 'react';
+
+export const AuthContext = createContext<{
+  isLoggedIn: boolean;
+  login: () => void;
+  logout: () => void;
+} | undefined>(undefined);
+
+export const Auth_provider = ({ children }: { children: ReactNode }): JSX.Element => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
