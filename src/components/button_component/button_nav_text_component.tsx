14d131466a73d222527ae5a9d509ded534576625
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-10-26 11:10:36 +0100

    feat: add button nav text component

---
+++ src/components/button_component/button_nav_text_component.tsx
@@ -0,0 +1,24 @@
+
+import { type VariantProps } from "class-variance-authority";
+import { type HTMLAttributes, type JSX } from "react";
+import { twMerge } from "tailwind-merge";
+import { buttonNavTextVariants } from "./button_nav_text_variants";
+
+type ButtonNavTextProps = HTMLAttributes<HTMLAnchorElement> &
+  VariantProps<typeof buttonNavTextVariants> & {
+    onClick?: () => void
+  };
+
+export const ButtonNavTextComponent =
+  ({ className, variant = "primary", onClick, ...props}: ButtonNavTextProps): JSX.Element => {
+    return (
+      <a
+        href="#"
+        className={twMerge(buttonNavTextVariants({ variant }), className)}
+        onClick={onClick}
+        {...props}
+      />
+    );
+  }
+
+ButtonNavTextComponent.displayName = "Text";
---
+++ src/components/button_component/button_nav_text_variants.ts
@@ -0,0 +1,13 @@
+import { cva } from "class-variance-authority";
+
+export const buttonNavTextVariants = cva("text-base cursor-pointer", {
+  variants: {
+    variant: {
+      primary: "font-semibold text-primary-500",
+      secondary: "font-semibold text-secondary-500",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+});
